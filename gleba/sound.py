0b4d5afc748e3f26581a5265378e10ded518e640
rotlug <rotemlugasi45@gmail.com>
2024-04-12 15:40:54 +0300

    Add volume option to sounds

--- gleba/sound.py
+++ gleba/sound.py
@@ -2,12 +2,14 @@ from gleba.core import *
 
 pygame.mixer.init()
 
-def play(path, loop=False):
+
+def play(path, volume=1, loop=False):
     sound = pygame.mixer.Sound(path)
     loops = 0
+
+    sound.set_volume(volume)
 
     if loop:
         loops = -1
 
     sound.play(loops=loops)
-
